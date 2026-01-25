25b34184bf923ede320ea4115d9d87b57d6ed4d2
Brunhub <bruno.rozov@gmail.com>
2026-01-25 23:55:07 +0200

    Update töö.py

--- Py töö/töö.py
+++ Py töö/töö.py
@@ -1,6 +1,66 @@
 #from import kaka siin, ALALALALA!
 from turtle import *
 from random import randint
+from tkinter import messagebox
+from time import sleep
+import sys
+import time
+
+#Roulette table
+def display_animation_casino():
+    tracer(0)
+    laud = Turtle()
+    laud.hideturtle()
+    laud.speed(0)
+    nool = Turtle()
+    nool.hideturtle()
+    nool.speed(0)
+    nool.color("white")
+    nool.width(4)
+    sektorid = 12
+    raadius = 120
+    nurk = 360 / sektorid
+    värvid = ["red", "black"] * (sektorid // 2)
+
+    
+
+    def joonista_rulet(pööre):
+        laud.clear()
+
+        for i in range(sektorid):
+            laud.penup()
+            laud.goto(0, 0)
+            laud.setheading(pööre + i * nurk)
+            laud.fillcolor(värvid[i])
+            laud.begin_fill()
+            laud.forward(raadius)
+            laud.left(90)
+            laud.circle(raadius, nurk)
+            laud.left(90)
+            laud.goto(0, 0)
+            laud.end_fill()
+
+    def joonista_nool():
+        nool.clear()
+        nool.penup()
+        nool.goto(0, raadius + 10)
+        nool.pendown()
+        nool.goto(0, raadius + 40)
+        
+    pööre = 0
+    kiirus = 100000
+
+    for _ in range(40):
+        pööre += kiirus
+        joonista_rulet(pööre)
+        joonista_nool()
+        update()
+        sleep(0.05)
+        kordaja = randint(100,1000)/1000
+        kiirus *= kordaja
+
+    tracer(1)
+
 # Ruut taust
 side = 600
 penup()
@@ -39,6 +99,16 @@ kood1 = textinput("PIN-sisestus", "Loo endale PIN-1:")
 kood2 = textinput("PIN-sisestus", "Loo endale PIN-2:")
 konto_tekst.clear()
 
+def neeger(sõnum, y_offset=0, viivitus=0.03):
+        tekst = ""
+        for täht in sõnum:
+            tekst += täht
+            konto_tekst.goto(-side/2 + 20, side/2 - 60 + y_offset)
+            konto_tekst.write(tekst, font=("Courier", 14, "normal"))
+            sleep(viivitus)
+
+
+
 # PIN-kontroll 3 katsega
 katseid = 3
 while katseid > 0:
@@ -75,16 +145,83 @@ except:
 
 if soovitud_raha <= konto:
     konto -= soovitud_raha
-    kiri(f"Võtsite {soovitud_raha} € välja!", -160)
-    kiri(f"Alles on {konto} €.", -190)
+    kiri(f"Võtsite {soovitud_raha} € välja!", -120)
+    kiri(f"Alles on {konto} €.", -320)
+        
+    raha = messagebox.askyesno("Küsimus", "Võta raha pangaautomaadist välja? (jah/ei)")
+        
+    if (raha):
+        screen = getscreen()
+        screen.addshape("raha.gif")
+        p = Turtle()
+        p.shape("raha.gif")
+        p.resizemode("user")
+        p.shapesize(0.5, 0.5)
+        p.penup()
+        p.goto(0, 0)
+       
+    else:
+        konto_tekst.clear()
+        ban = nimi + " has been givin " + str(soovitud_raha) + "€" + " for good behavior"
+        kiri("Pangaautomaat: Olgu, ma jätan endale siis", 30)
+        sleep(2)
+        kiri(nimi + ": Nooooo! Anna tagasi :(", 10)
+        sleep(1)
+        kiri("Pangaautomaat: Ei...", -10)
+        sleep(2)
+        kiri(nimi + ": Aga miks?", -30)
+        sleep(2)
+        kiri("Pangaautomaat: Sa oled ülbe neeger", -50)
+        sleep(1.5)
+        kiri(nimi + ": Ei mai ole >:(", -70)
+        sleep(0.5)
+        kiri("User20384: First lol", -90)
+        sleep(1.5)
+        kiri(nimi + ": Kes see ahv veel on?", -110)
+        sleep(2)
+        kiri("Pangaautomaat: Hold up, las ma bannin ta", -130)
+        sleep(1.75)
+        kiri("Server: User20384 got kick reason spamming ", -150)
+        sleep(2)
+        kiri(nimi + ": Anna mu " + str(soovitud_raha) + "€ ka tagasi nüüd", -170)
+        sleep(1.75)
+        kiri("Pangaautomaat: No okei, kuna su emme ütles", -190)
+        sleep(2)
+        neeger("Server: " + ban, -210)
+        sleep(1)
+        kiri(nimi + ": Yay! thank you mommy", -230)
+        sleep(1.5)
+        kiri("Pangaautomaat: Edaspidi oled tubli poiss, eks?", -250)
+        sleep(1.75)
+        tubli_poiss = messagebox.askyesno("Tähtis küsimus", "Kas sa oled edaspidi tubli poiss? (Jah issi/Ei)")
+        konto_tekst.clear()
+
+        if (tubli_poiss):
+            kiri("Tublu poiss", 30)
+            sleep(1)
+            kiri("Pangaautomaat: Võta oma " + str(soovitud_raha), 10)
+            sleep(1.5)
+            kiri(nimi + ": Väga äge", -30)
+
+        else:
+            kiri("Pangaautomaat: ...", 30)
+            sleep(2)
+            kiri("Pangaautomaat: Mida vittu sa ütlesid mulle?", 10)
+            sleep(1.5)
+            kiri(nimi + ": Ma ei ole su issi🗣️💢💢", -10)
+
+        
 else:
-    kiri("Kontol on liiga vähe raha!", -160)
-    casino = textinput("Kasiino", "Kas soovite teenida raha juurde? (jah/ei)")
-    if casino and casino.lower() == "jah":
+    kiri("Kontol on liiga vähe raha!", -130)
+
+    casino = messagebox.askyesno("Kasiino", "Kas soovite teenida raha juurde?")
+
+    if casino:
         kasutaja_valik = textinput("Kasiino", "Red (1) või Black (2)?")
         konto_tekst.clear()
         try:
             kasutaja_valik = int(kasutaja_valik)
+            display_animation_casino()
         except:
             kiri("Vale sisestus!", -190)
             exitonclick()
@@ -101,7 +238,6 @@ else:
             p.shapesize(0.5, 0.5)
             p.penup()
             p.goto(0, 0)
-           
         else:
             konto = 0
             kiri("Sa kaotasid kogu oma raha :(", 20)
@@ -114,6 +250,7 @@ else:
             p.penup()
             p.goto(0, 0)
     else:
-        kiri("Ole vaene rott edasi siis...", -190)
-
+        konto_tekst.clear()
+        kiri("Ole vaene rott edasi siis...", 20)
+        
 exitonclick()
